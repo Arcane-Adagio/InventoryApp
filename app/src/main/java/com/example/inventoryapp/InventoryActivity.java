5470282759a168a7cd99e5a0a1268360ab2992d2
Patrick L Coleman Jr <drpjc99@gmail.com>
2022-08-11 08:35:03 -0400

    adds inventory menu

---
+++ app/src/main/java/com/example/inventoryapp/InventoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.inventoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
